1b11d897e0d55b74471772e6727ce596d898665a
satish Rajbanshi <satish_95raj@outlook.com>
2021-11-04 08:44:10 +0530

    second commit

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/task');
-const url = 'mongodb://localhost/Test'
+const express = require("express"); //For using route using express
+const mongoose = require("mongoose"); //For creating Models and Making connection with mongodb(database)
+const userRoutes = require("./routes/task"); //import route
+const url = "mongodb://localhost/Test";
 
 const app = express();
-const PORT=9000;
+const PORT = 9000;
 
-mongoose.connect(url, {useNewUrlParser:true})
-const con = mongoose.connection
+mongoose.connect(url, { useNewUrlParser: true });
+const con = mongoose.connection;
 
-con.on('open', () => {
-    console.log('connected...')
-})
+con.on("open", () => {
+  console.log("connected...");
+});
 
 app.use(express.json());
 
-app.use('/task',userRoutes);
-
-
+app.use("/task", userRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server started on port: http://localhost:${PORT}`);
-})
+  console.log(`Server started on port: http://localhost:${PORT}`);
+});
--- models/product.js
+++ models/product.js
@@ -1,38 +1,35 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
+  productId: {
+    type: String,
+    required: true,
+  },
+  productName: {
+    type: String,
+    required: true,
+  },
+  qtyPerUnit: {
+    type: Number,
+    required: true,
+  },
+  unitPrice: {
+    type: Number,
+    required: true,
+  },
+  unitInStock: {
+    type: Number,
+    required: true,
+  },
+  discontinued: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  categoryId: {
+    type: String,
+    required: true,
+  },
+});
 
-    productId: {
-        type: String,
-        required: true
-    },
-    productName: {
-        type: String,
-        required: true
-    },
-    qtyPerUnit:{
-        type:Number,
-        required:true
-    },
-    unitPrice:{
-        type:Number,
-        required:true
-    },
-    unitInStock:{
-        type:Number,
-        required:true
-    },
-    discontinued: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    categoryId:{
-        type:String,
-        required:true
-    },
-
-})
-
-module.exports = mongoose.model('Product',productSchema)
+module.exports = mongoose.model("Product", productSchema);
